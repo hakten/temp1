5d1ae4562da22098a944fed32d4da7a6075b9a41
Huseyin Akten <husakten@gmail.com>
2019-12-29 15:02:23 -0600

    init push

--- sg.tf
+++ sg.tf
@@ -8,7 +8,19 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port       = 0
     to_port         = 0
