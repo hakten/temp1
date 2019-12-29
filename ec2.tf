32913ff40b811c37e6bae7c095b29becbe118c50
Huseyin Akten <husakten@gmail.com>
2019-12-28 21:08:37 -0600

    dsaf

--- ec2.tf
+++ ec2.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "web" {
   ami           = "ami-0b2d8d1abb76a53d8"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
-  security_groups = ["ssh"]
+  security_groups = [aws_security_group.ssh.id]
 }
