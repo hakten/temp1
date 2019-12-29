477dc33bd0666181aab351599e1137637d3de7e6
Huseyin Akten <husakten@gmail.com>
2019-12-29 15:09:27 -0600

    sffs

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b2d8d1abb76a53d8"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
   subnet_id     = aws_subnet.main.id
   associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.ssh.id]
