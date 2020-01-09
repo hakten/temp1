d6057e2a8a60389bb8ccc43e765f9eaaed9a1c16
Huseyin Akten <husakten@gmail.com>
2020-01-09 14:08:19 -0600

    asfd

--- vpc-module.tf
+++ vpc-module.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/hakten/terraform-resources.git"
+  source = "git::https://github.com/hakten/terraform-resources.git/vpc-module"
 
 cidr            = "10.0.0.0/16"
 name            = "Wordpress"
