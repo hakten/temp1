ac607d60953ea51c955994189c9391b6495a43a0
Huseyin Akten <husakten@gmail.com>
2020-01-09 15:35:33 -0600

    outputs edited

--- vpc-module.tf
+++ vpc-module.tf
@@ -9,7 +9,8 @@ azs             = ["us-east-1a","us-east-1b","us-east-1c"]
 public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
 private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
 
-output "vpc_id" {
-  value = module.vpc.vpc-id
 }
+
+output "vpc_id" {
+  value = "${module.vpc.vpc-id}"
 }
