26ab040c9234f343707edcb988f2965d743da731
Huseyin Akten <husakten@gmail.com>
2020-01-09 02:45:20 -0600

    git

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 module "vpc_example_simple-vpc" {
-  source  = "github.com/terraform-aws-modules/terraform-aws-vpc/tree/v2.21.0/examples/simple-vpc"
+  source  = "https://github.com/terraform-aws-modules/terraform-aws-vpc/tree/v2.21.0/examples/simple-vpc"
   version = "2.21.0"
 }
 
