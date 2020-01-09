6597c96bd497a0278cccfca32ef7b088618f9446
Huseyin Akten <husakten@gmail.com>
2020-01-09 15:23:00 -0600

    output added

--- vpc-module.tf
+++ vpc-module.tf
@@ -10,3 +10,7 @@ public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
 private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
 
 }
+
+output "vpc_id" {
+  value = "module.vpc.aws_vpc.vpc.id"
+}
