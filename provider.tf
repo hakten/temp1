a62bef77d4a917d076d9fe04cfd541ec19f69a98
Huseyin Akten <husakten@gmail.com>
2020-01-09 02:42:19 -0600

    region changed

--- provider.tf
+++ provider.tf
@@ -1,8 +1,9 @@
 module "vpc_example_simple-vpc" {
   source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
   version = "2.21.0"
-}
-
-provider "aws" {
+  provider "aws" {
   region = "us-west-1"
 }
+}
+
+
