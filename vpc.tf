7f27ef7a106fd2791a56d6e84b45763c3590480a
Huseyin Akten <husakten@gmail.com>
2020-01-11 17:48:43 -0600

    nat adjusted to multiple zones

--- vpc.tf
+++ vpc.tf
@@ -23,8 +23,8 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_subnet" "private_subnets" {
   vpc_id                  = "${aws_vpc.vpc.id}"
   count                   = "${length(var.azs)}"
-  availability_zone       = "${element(var.azs,count.index)}"
-  cidr_block              = "${element(var.private_subnets,count.index)}"
+  availability_zone       = "${var.azs[count.index]}"
+  cidr_block              = "${var.private_subnets[count.index]}"
   map_public_ip_on_launch = false
   
   tags = {
@@ -32,31 +32,31 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = "${aws_vpc.vpc.id}"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
 
-#   tags = {
-#     Name = "${var.environment}-Internet_Gateway"
-#   }
-# }
+  tags = {
+    Name = "${var.environment}-Internet_Gateway"
+  }
+}
 
-# resource "aws_route_table" "public_route_table" {
-#   vpc_id = "${aws_vpc.vpc.id}"
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${aws_vpc.vpc.id}"
 
-#   route {
-#     cidr_block  = "0.0.0.0/0"
-#     gateway_id  = "${aws_internet_gateway.igw.id}"
-#   }
-#     tags = {
-#     Name = "${var.environment}-Public_Route_Table"
-#   }
-# }
+  route {
+    cidr_block  = "0.0.0.0/0"
+    gateway_id  = "${aws_internet_gateway.igw.id}"
+  }
+    tags = {
+    Name = "${var.environment}-Public_Route_Table"
+  }
+}
 
-# resource "aws_route_table_association" "public_route_table_association" {
-#   route_table_id = "${aws_route_table.public_route_table.id}"
-#   subnet_id      = "${aws_subnet.public_subnets[count.index].id}"
-#   count          = "${length(var.azs)}"
-# }
+resource "aws_route_table_association" "public_route_table_association" {
+  route_table_id = "${aws_route_table.public_route_table.id}"
+  subnet_id      = "${aws_subnet.public_subnets[*].id}"
+  # count          = "${length(var.azs)}"
+}
 
 # resource "aws_eip" "eip" {
 #   vpc      = true
