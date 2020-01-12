0f17873fe189ac561fde046b441c0d0af76c502f
Huseyin Akten <husakten@gmail.com>
2020-01-11 21:57:18 -0600

    vpc

--- vpc.tf
+++ vpc.tf
@@ -67,7 +67,7 @@ resource "aws_eip" "eip" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  subnet_id     = "${aws_subnet.public_subnets[0]}.id"
+  subnet_id     = "${aws_subnet.public_subnets.0.id}"
   allocation_id = "${aws_eip.eip.id}"
 
   tags = {
