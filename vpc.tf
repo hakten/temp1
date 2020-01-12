32ebc538c35634623afe0b7b7c0a55fbf296baa3
Huseyin Akten <husakten@gmail.com>
2020-01-11 21:43:30 -0600

    vpc

--- vpc.tf
+++ vpc.tf
@@ -20,75 +20,75 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
-resource "aws_subnet" "private_subnets" {
-  vpc_id                  = "${aws_vpc.vpc.id}"
-  count                   = "${length(var.private_subnets)}"
-  availability_zone       = "${element(var.azs,count.index)}"
-  cidr_block              = "${var.private_subnets[count.index]"
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "private_subnets" {
+#   vpc_id                  = "${aws_vpc.vpc.id}"
+#   count                   = "${length(var.private_subnets)}"
+#   availability_zone       = "${element(var.azs,count.index)}"
+#   cidr_block              = "${var.private_subnets[count.index]"
+#   map_public_ip_on_launch = false
   
-  tags = {
-    Name = "${var.environment}-Private_Subnet-${count.index+1}"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-
-  tags = {
-    Name = "${var.environment}-Internet_Gateway"
-  }
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = "${aws_vpc.vpc.id}"
-
-  route {
-    cidr_block  = "0.0.0.0/0"
-    gateway_id  = "${aws_internet_gateway.igw.id}"
-  }
-    tags = {
-    Name = "${var.environment}-Public_Route_Table"
-  }
-}
-
-# resource "aws_route_table_association" "public_route_table_association" {
-#   route_table_id = "${aws_route_table.public_route_table.id}"
-#   subnet_id      = "${element(var.public_subnets.*.id,count.index}"
-#   count          = "${length(var.public_subnets)}"
-# }
-
-# resource "aws_eip" "eip" {
-#   vpc      = true
-
 #   tags = {
-#     Name = "${var.environment}-Elastic_IP"
+#     Name = "${var.environment}-Private_Subnet-${count.index+1}"
 #   }
 # }
 
-# resource "aws_nat_gateway" "nat" {
-#   subnet_id     = "${aws_subnet.public_subnets[0].id}"
-#   allocation_id = "${aws_eip.eip.id}"
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = "${aws_vpc.vpc.id}"
 
 #   tags = {
-#     Name = "${var.environment}-Nat_Gateway"
+#     Name = "${var.environment}-Internet_Gateway"
 #   }
 # }
 
-# resource "aws_route_table" "private_route_table" {
+# resource "aws_route_table" "public_route_table" {
 #   vpc_id = "${aws_vpc.vpc.id}"
 
 #   route {
-#     cidr_block     = "0.0.0.0/0"
-#     nat_gateway_id = "${aws_nat_gateway.nat.id}"
+#     cidr_block  = "0.0.0.0/0"
+#     gateway_id  = "${aws_internet_gateway.igw.id}"
 #   }
 #     tags = {
-#     Name = "${var.environment}-Private_Route_Table"
+#     Name = "${var.environment}-Public_Route_Table"
 #   }
 # }
 
-# resource "aws_route_table_association" "private_route_table_association" {
-#   route_table_id = "${aws_route_table.private_route_table.id}"
-#   subnet_id      = "${aws_subnet.private_subnets[count.index].id}"
-#   count          = "${length(var.azs)}"
-# }
+# # resource "aws_route_table_association" "public_route_table_association" {
+# #   route_table_id = "${aws_route_table.public_route_table.id}"
+# #   subnet_id      = "${element(var.public_subnets.*.id,count.index}"
+# #   count          = "${length(var.public_subnets)}"
+# # }
+
+# # resource "aws_eip" "eip" {
+# #   vpc      = true
+
+# #   tags = {
+# #     Name = "${var.environment}-Elastic_IP"
+# #   }
+# # }
+
+# # resource "aws_nat_gateway" "nat" {
+# #   subnet_id     = "${aws_subnet.public_subnets[0].id}"
+# #   allocation_id = "${aws_eip.eip.id}"
+
+# #   tags = {
+# #     Name = "${var.environment}-Nat_Gateway"
+# #   }
+# # }
+
+# # resource "aws_route_table" "private_route_table" {
+# #   vpc_id = "${aws_vpc.vpc.id}"
+
+# #   route {
+# #     cidr_block     = "0.0.0.0/0"
+# #     nat_gateway_id = "${aws_nat_gateway.nat.id}"
+# #   }
+# #     tags = {
+# #     Name = "${var.environment}-Private_Route_Table"
+# #   }
+# # }
+
+# # resource "aws_route_table_association" "private_route_table_association" {
+# #   route_table_id = "${aws_route_table.private_route_table.id}"
+# #   subnet_id      = "${aws_subnet.private_subnets[count.index].id}"
+# #   count          = "${length(var.azs)}"
+# # }
