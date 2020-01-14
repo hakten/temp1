ee3a1cc325eaf4b9642313fe8a3e2e4720ec97d0
Huseyin Akten <husakten@gmail.com>
2020-01-14 04:01:18 -0600

    sdfa

--- vpc-module/22-bastion.tf
+++ vpc-module/22-bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
   instance_type          = "t2.micro"
   ami                    = "ami-00068cd7555f543d5"
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.public_subs[0]
   key_name               = "My Mac"
   
   #user_data = file("")
