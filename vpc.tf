471165510840d9ffea93c957f44edb5e54fb3b41
Huseyin Akten <husakten@gmail.com>
2020-02-02 15:39:40 -0600

    push

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 
 resource "aws_vpc_peering_connection" "foo" {
-  peer_owner_id = "${data.aws_caller_identity.current}"
+  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
   peer_vpc_id   = "${aws_vpc.bar.id}"
   vpc_id        = "${aws_vpc.foo.id}"
   auto_accept   = true
