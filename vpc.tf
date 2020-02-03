dbfb07b02d225dc95957358ca688494ad5ca1795
Huseyin Akten <husakten@gmail.com>
2020-02-03 00:22:03 -0600

    sadf

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,20 @@
-resource "aws_vpc" "foo" {
-  provider "aws" {
+provider "aws" {
+  alias = "veer"
   region = "us-east-1"
 }
+
+resource "aws_vpc" "foo" {
+  provider = "aws.veer"
   cidr_block = "10.1.0.0/16"
 }
 
-resource "aws_vpc" "bar" {
-  provider "aws" {
-  region = "us-east-1"
+provider "aws" {
+  alias = "peer"
+  region = "us-east-2"
 }
+
+resource "aws_vpc" "bar" {
+  provider = "aws.peer"
   cidr_block = "10.2.0.0/16"
 }
 
