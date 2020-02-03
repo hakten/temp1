64a3bf76b91aa56307d691574767ddd4d3296c19
Huseyin Akten <husakten@gmail.com>
2020-02-03 00:18:05 -0600

    dsfg

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,15 @@
-provider "aws" {
+resource "aws_vpc" "foo" {
+  provider "aws" {
   region = "us-east-1"
 }
-
-
-resource "aws_vpc_peering_connection" "foo" {
-  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
-  peer_vpc_id   = "${aws_vpc.bar.id}"
-  vpc_id        = "${aws_vpc.foo.id}"
-  auto_accept   = true
-
-  tags = {
-    Name = "VPC Peering between foo and bar"
-  }
-}
-
-resource "aws_vpc" "foo" {
   cidr_block = "10.1.0.0/16"
 }
 
 resource "aws_vpc" "bar" {
+  provider "aws" {
+  region = "us-east-1"
+}
   cidr_block = "10.2.0.0/16"
 }
 
-data "aws_caller_identity" "current" {}
+
