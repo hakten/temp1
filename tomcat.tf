f396e7dba5fa42cb85b60b0affb5a0f23fea56e0
Huseyin Akten <husakten@gmail.com>
2020-02-16 13:56:34 -0600

    sdf

--- tomcat.tf
+++ tomcat.tf
@@ -78,29 +78,18 @@ resource "aws_instance" "tomcat" {
         "echo JAVA_HOME=`find /usr/lib/jvm/java-1.8* | head -n 3|tail -n1` >> ~/.bash_profile",
         "cd /opt && sudo yum install wget -y &&",
         "sudo wget -P /opt https://downloads.apache.org/tomcat/tomcat-8/v8.5.51/bin/apache-tomcat-8.5.51.tar.gz &&",
-        "sudo tar -xzf /opt/apache-tomcat-8.5.51.tar.gz &&",
+        "sudo tar -xvzf /opt/apache-tomcat-8.5.51.tar.gz &&",
         "sudo cp -f /tmp/context.xml /opt/apache-tomcat-8.5.51/webapps/manager/META-INF/context.xml &&",
         "sudo cp -f /tmp/context.xml /opt/apache-tomcat-8.5.51/webapps/host-manager/META-INF/context.xml &&",
         "sudo cp -f /tmp/tomcat-users.xml /opt/apache-tomcat-8.5.51/conf/tomcat-users.xml &&",
         "sudo rm -f /tmp/context.xml &&",
         "sudo rm -f /tmp/tomcat-users.xml &&",
         "sudo chmod 775 /opt/apache-tomcat-8.5.51/bin/shutdown.sh &&",
-        "sudo chmod 775 /opt/apache-tomcat-8.5.51/bin/startup.sh &&"
+        "sudo chmod 775 /opt/apache-tomcat-8.5.51/bin/startup.sh &&",
+        "sudo sleep 10 &&",
+        "sudo /opt/apache-tomcat-8.5.51/bin/startup.sh"
         
         ]
       } 
-
-      provisioner "remote-exec" {
-    connection {
-      host        = self.public_ip
-      type        = "ssh"
-      user        = "centos"
-      private_key = file("~/.ssh/id_rsa")
-      }
-      inline = [
-        "sudo /opt/apache-tomcat-8.5.51/bin/startup.sh",
-        "sleep 10"
-      ]
-      }
    
 }
